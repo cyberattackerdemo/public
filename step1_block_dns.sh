f45593839c2cb357201cf12fd317873368cdcc8d
cyberattackerdemo <cyber.attacker.demo@gmail.com>
2025-06-19 21:34:10 +0900

    Update step1_block_dns.sh

--- step1_block_dns.sh
+++ step1_block_dns.sh
@@ -19,11 +19,7 @@ dig cybereason.net @127.0.0.1 | tee -a $LOG_FILE
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') | ===== step1_block_dns.sh completed =====" | tee -a $LOG_FILE
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') | Capturing DNS traffic with tcpdump for 10 sec..." | tee -a $LOG_FILE
-timeout 300 tcpdump -i any port 53 -w /home/troubleshoot/step1_tcpdump_dns.pcap
-echo "$(date '+%Y-%m-%d %H:%M:%S') | tcpdump capture saved: /home/troubleshoot/step1_tcpdump_dns.pcap" | tee -a $LOG_FILE
-
 # 最後に tcpdump を自動キャプチャ (300秒)
 echo "$(date '+%Y-%m-%d %H:%M:%S') | Capturing DNS traffic with tcpdump for 300 sec..." | tee -a $LOG_FILE
-timeout 300 tcpdump -i any port 53 -w /home/troubleshoot/step1_tcpdump_dns.pcap
+sudo timeout 300 tcpdump -i any port 53 -w /home/troubleshoot/step1_tcpdump_dns.pcap
 echo "$(date '+%Y-%m-%d %H:%M:%S') | tcpdump capture saved: /home/troubleshoot/step1_tcpdump_dns.pcap" | tee -a $LOG_FILE
