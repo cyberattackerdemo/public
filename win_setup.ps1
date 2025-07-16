ed75d2f044026f1143ead94feb80a9510c8665bb
cyberattackerdemo <cyber.attacker.demo@gmail.com>
2025-07-16 15:10:42 +0900

    Add files via upload

--- win_setup.ps1
+++ win_setup.ps1
@@ -20,6 +20,31 @@ function LogError($message) {
 Start-Transcript -Path "C:\win_setup_transcript.txt" -Append
 Log "スクリプト実行開始"
 
+try {
+    Log "初期セットアップ用日本語UIスクリプトをGitHubからダウンロード"
+    $publicPath = "C:\Users\Public"
+    $publicDesktop = "C:\Users\Public\Desktop"
+    $setupScriptUrl = "https://raw.githubusercontent.com/cyberattackerdemo/public/main/setup_Japanese_windows10.ps1"
+    $setupScriptPath = Join-Path $publicPath "setup_Japanese_windows10.ps1"
+
+    Invoke-WebRequest -Uri $setupScriptUrl -OutFile $setupScriptPath -UseBasicParsing
+    Log "セットアップスクリプトのダウンロード完了: $setupScriptPath"
+} catch {
+    LogError "セットアップスクリプトのダウンロード失敗: $($_.Exception.Message)"
+}
+
+try {
+    Log "セットアップ用BATファイルをPublic\Desktopに作成"
+    $batPath = Join-Path $publicDesktop "Run_setup_Japanese_windows10.bat"
+
+    $batContent = "@echo off`r`npowershell -ExecutionPolicy Bypass -File `"%setupScriptPath%`"`r`npause"
+    Set-Content -Path $batPath -Value $batContent -Encoding ASCII -Force
+
+    Log "BATファイル作成完了: $batPath"
+} catch {
+    LogError "BATファイル作成失敗: $($_.Exception.Message)"
+}
+
 # Defender無効化
 try {
     Log "Defenderのリアルタイム保護を無効化"
@@ -231,33 +256,6 @@ try {
         LogError "Outlook 実行ファイルが見つかりませんでした"
     }
 } catch { LogError "Outlookショートカット作成失敗: $($_.Exception.Message)" }
-
-try {
-    Log "次回セットアップ用スクリプトをGitHubからデスクトップにダウンロード"
-    $desktop = [Environment]::GetFolderPath("Desktop")
-    $setupScriptUrl = "https://raw.githubusercontent.com/cyberattackerdemo/public/main/setup_Japanese_windows10.ps1"
-    $setupScriptPath = Join-Path $desktop "setup_Japanese_windows10.ps1"
-
-    Invoke-WebRequest -Uri $setupScriptUrl -OutFile $setupScriptPath -UseBasicParsing
-    Log "セットアップスクリプトのダウンロード完了: $setupScriptPath"
-} catch {
-    LogError "セットアップスクリプトのダウンロード失敗: $($_.Exception.Message)"
-}
-
-try {
-    Log "セットアップ用BATファイルをデスクトップに作成"
-    $desktop = [Environment]::GetFolderPath("Desktop")
-    $batPath = Join-Path $desktop "Run_setup_Japanese_windows10.bat"
-    $ps1Path = Join-Path $desktop "setup_Japanese_windows10.ps1"
-
-    $batContent = "@echo off`r`npowershell -ExecutionPolicy Bypass -File `"$ps1Path`"`r`npause"
-    Set-Content -Path $batPath -Value $batContent -Encoding ASCII -Force
-
-    Log "BATファイル作成完了: $batPath"
-} catch {
-    LogError "BATファイル作成失敗: $($_.Exception.Message)"
-}
-
 
 # 一時フォルダ削除
 try {
